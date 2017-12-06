84ac51aa46cca7c5d9ca1eab8a7ab1f3fe1b36da
Verena <verena.gu@aon.at>
2017-12-06 09:09:33 +0100

    Datenbank Methoden

--- SwpSepmPorject/src/model/DatenbankUnicorn.java
+++ SwpSepmPorject/src/model/DatenbankUnicorn.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.sql.Array;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -20,10 +21,13 @@ public class DatenbankUnicorn {
 	static Connection conn = null;
 	static Statement stmt = null;
 	static PreparedStatement pstmt = null;
+	static ResultSet rs = null;
 
 
 	public static void main(String[] args) {
 		VerbindungAufbauen();
+		UnderworldinDB();
+		OverworldinDB();
 
 	}
 
@@ -39,28 +43,124 @@ public class DatenbankUnicorn {
 	}
 
 
-	
+
 
 	// TODO
-	public static void UnderworldinDB()
+	public static ArrayList<String[]> OverworldinDB()
 	{
-		
-		ArrayList<String[]> spielfeld = new ArrayList<String[]>();
-		
-		String sql_OworldinDB="Select odiif, xKoordinaten, yKoordinaten, username from OverworldField Join Spieler on username";
+
+		ArrayList<String[]> spielfeldOW = new ArrayList<String[]>();
+
+		String sql_OworldinDB="Select owfid, xKoordinaten, yKoordinaten, username, owid from overworldField Full Join overworld USING (owid)";
 		try {
-			stmt = conn.createStatement();
-			ResultSet rs=stmt.executeQuery(sql_OworldinDB);
-			
+			conn=DriverManager.getConnection(DB_URL);
+			pstmt = conn.prepareStatement(sql_OworldinDB);
+			rs=pstmt.executeQuery(sql_OworldinDB);
+
+			System.out.println("Overworld ausgeben:");
 			while(rs.next()){
-				
-				
+
+				String daten[]=new String[5];
+				System.out.print("Gelesen wurde: ");
+				for (int i = 0; i < 10; i++) {
+					daten[i] = rs.getString(i+1);
+					System.out.print(" '" + daten[i] + "'");	//zur Kontrolle
+				}
+				spielfeldOW.add(daten);
 			}
-			
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+		//return null;
+
+		return spielfeldOW;
+	}
+
+	public static ArrayList<String[]> UnderworldinDB()
+	{
+
+		ArrayList<String[]> spielfeldUW = new ArrayList<String[]>();
+
+		String sql_UworldinDB="Select udfid, xKoordinaten, yKoordinaten, gebaeudeid, uwid, username from underworld Join underworldfield using(uwid)";
+		try{
+
+			conn=DriverManager.getConnection(DB_URL);
+			pstmt = conn.prepareStatement(sql_UworldinDB);
+			rs=pstmt.executeQuery();
+
+			System.out.println("Underworld ausgeben:");
+			while(rs.next()){
+
+				String daten[]=new String[5];
+				System.out.print("Gelesen wurde: ");
+				for (int i = 0; i < 10; i++) {
+					daten[i] = rs.getString(i+1);
+					System.out.print(" '" + daten[i] + "'");	//zur Kontrolle
+				}
+				spielfeldUW.add(daten);
+			}
+
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		//return null;
+
+		return spielfeldUW;
+	}
+	
+	public static boolean writeFeldUW(String[] testzeile2) {
+
+		boolean erfolg = true;
+		//SQL-Abfrag zum hineinschreiben neuer Daten
+		String INSERT_DATA_SQL = "INSERT INTO underworldfield ( xKoordinaten, yKoordinaten, uwid, udfid, gebaeudeid) VALUES (?, ?, ?, ?, ?)";
+
+		//connection Aufbau
+		try {
+			conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			pstmt = conn.prepareStatement(INSERT_DATA_SQL);
+
+			//System.out.println("HIIIIIII"); //zur Kontrolle
+
+			for (int i = 1; i <= 6; i++) {
+				pstmt.setString(i, testzeile2[i-1]);
+				System.out.println(" '" + testzeile2[i-1] + "'");
+			}
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			erfolg = false;
+		}
+		return erfolg;
+
+	}
+	
+	public static boolean writeFeldOW(String[] testzeile2) {
+
+		boolean erfolg = true;
+		//SQL-Abfrag zum hineinschreiben neuer Daten
+		String INSERT_DATA_SQL = "INSERT INTO overworldfield ( xKoordinaten, yKoordinaten, owid, owfid, username) VALUES (?, ?, ?, ?, ?)";
+
+		//connection Aufbau
+		try {
+			conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			pstmt = conn.prepareStatement(INSERT_DATA_SQL);
+
+			//System.out.println("HIIIIIII"); //zur Kontrolle
+
+			for (int i = 1; i <= 6; i++) {
+				pstmt.setString(i, testzeile2[i-1]);
+				System.out.println(" '" + testzeile2[i-1] + "'");
+			}
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			erfolg = false;
+		}
+		return erfolg;
+
 	}
 }
